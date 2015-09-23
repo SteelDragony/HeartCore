f4730ce6e41b03e22ac84ff7ab8b6408c02b8580
SteelDragony <blaweetniet@live.nl>
2015-09-23 21:52:00 +0200

    laptop version push

---
+++ Assets/Scripts/FrequencyMeasure.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrequencyMeasure : MonoBehaviour {
+
+	ArrayList intervals;
+
+	bool pressed = false;
+	float lastTimePressed = 0f;
+
+	// Use this for initialization
+	void Start () {
+		intervals = new ArrayList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pressed = false;
+		if( Input.GetKeyDown( KeyCode.UpArrow))
+		{
+			pressed = true;
+			float now = Time.time;
+			float delta = now - lastTimePressed;
+			lastTimePressed = now;
+			intervals.Add (delta);
+
+			if(intervals.Count > 10)
+				intervals.RemoveAt(0);
+			CalculateAvarage();
+		}
+	}
+
+	void CalculateAvarage()
+	{
+		if(intervals.Count < 2)
+			return;
+
+		float total = 0;
+
+		for (int i = 0; i < intervals.Count-1; i++) {
+			total += (float) intervals[i];
+		}
+
+		float avg = total / (intervals.Count-1);
+
+		Debug.Log (1/avg * 60);
+	}
+}
---
+++ Assets/Scripts/buttonpressing.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonpressing : MonoBehaviour {
+
+	public float score = 0f;
+	public float amplitude = 1f;
+	public float frequency = 1f;
+
+	public float sinwave = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sinwave = Mathf.Sin(Time.time * frequency * 2 * Mathf.PI) * amplitude;
+		if(Input.GetButtonDown("Fire1"))
+		{
+			score += sinwave;
+		}
+
+	}
+}
