5d4ad4b909f8ea9b2c5b8dcfaf1328962a42ec3b
SteelDragony <blaweetniet@live.nl>
2015-10-09 15:06:06 +0200

    updated frequency measure and alarmlights

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+
+
+    public float frequency = 60f;
+    public float amlitude = 1f;
+    Light alarmLight;
+
+	// Use this for initialization
+	void Start () {
+        alarmLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        alarmLight.intensity = Mathf.Sin(Time.time * frequency) * amlitude + amlitude;
+	}
+}
--- Assets/Scripts/FrequencyMeasure.cs
+++ Assets/Scripts/FrequencyMeasure.cs
@@ -1,48 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FrequencyMeasure : MonoBehaviour {
+public class FrequencyMeasure : MonoBehaviour
+{
 
-	ArrayList intervals;
+    ArrayList intervals;
 
-	bool pressed = false;
-	float lastTimePressed = 0f;
+    bool pressed = false;
+    float lastTimePressed = 0f;
 
-	// Use this for initialization
-	void Start () {
-		intervals = new ArrayList();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		pressed = false;
-		if( Input.GetKeyDown( KeyCode.UpArrow))
-		{
-			pressed = true;
-			float now = Time.time;
-			float delta = now - lastTimePressed;
-			lastTimePressed = now;
-			intervals.Add (delta);
+    public float targetDifference = 0.6f;
+    public float accumulatedError = 0f;
+    public float errorDecayRate = 0.1f;
 
-			if(intervals.Count > 2)
-				intervals.RemoveAt(0);
-			CalculateAvarage();
-		}
-	}
+    public AlarmLight alarmLight;
 
-	void CalculateAvarage()
-	{
-		if(intervals.Count < 2)
-			return;
+    // Use this for initialization
+    void Start()
+    {
+        intervals = new ArrayList();
+    }
 
-		float total = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        pressed = false;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pressed = true;
+            float now = Time.time;
+            float delta = now - lastTimePressed;
+            lastTimePressed = now;
+            intervals.Add(delta);
 
-		for (int i = 0; i < intervals.Count-1; i++) {
-			total += (float) intervals[i];
-		}
+            accumulatedError += Mathf.Abs(delta - targetDifference);
 
-		float avg = total / (intervals.Count-1);
+            if (intervals.Count > 3)
+                intervals.RemoveAt(0);
+            CalculateAvarage();
+        }
+        /*
+        if (accumulatedError > 0)
+        {
+            accumulatedError -= errorDecayRate * Time.deltaTime;
+        }
+        alarmLight.amlitude = accumulatedError;
+        */
+    }
 
-		Debug.Log (1/avg * 60);
-	}
+    void CalculateAvarage()
+    {
+        if (intervals.Count < 2)
+            return;
+
+        float total = 0;
+
+        for (int i = 0; i < intervals.Count - 1; i++)
+        {
+            total += (float)intervals[i];
+        }
+
+        float avg = total / (intervals.Count - 1);
+        float frequency = 1 / avg * 60;
+        alarmLight.amlitude = Mathf.Abs(frequency - 104f)/10;
+
+        Debug.Log(frequency);
+        Debug.Log(avg);
+    }
 }
