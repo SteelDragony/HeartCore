d63427d61fd9a5cb41c5a88aa1e7c01874753b0c
SteelDragony <blaweetniet@live.nl>
2015-10-29 21:27:12 +0100

    first Door open script WIP

---
+++ Assets/BackDoorOpenScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackDoorOpenScript : MonoBehaviour {
+
+    public bool doorOpen = false;
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    Vector3 leftDoorStart;
+    Vector3 leftDoorOpen;
+    Vector3 rightDoorStart;
+    Vector3 rightDoorOpen;
+
+
+	// Use this for initialization
+	void Start () {
+        leftDoorStart = leftDoor.transform.position;
+        leftDoorOpen = leftDoorStart;
+        leftDoorOpen.z += 5;
+        rightDoorStart = rightDoor.transform.position;
+        rightDoorOpen = rightDoorStart;
+        rightDoorOpen.z -= 5;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(doorOpen)
+        {
+            leftDoor.transform.position = leftDoorOpen; 
+        }
+        else
+        {
+            leftDoor.transform.position = leftDoorStart;
+        }
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        doorOpen = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        doorOpen = false;
+    }
+}
